5a25ccd889b0ce2c481bb089ad91c2641d776f06
Kendra Burbank <burbank@uchicago.edu>
2024-11-05 13:29:42 -0600

    combines different words in the same line into single rawinlines in the ast. still works.

--- _extensions/examclass/latexComments.lua
+++ _extensions/examclass/latexComments.lua
@@ -12,7 +12,6 @@ function Math(el)
    -- Check if el is valid and has text property
    if el and el.text then
     if el.text:match("^%s%%") then
-        io.stderr:write("We have a comment in math" .. el.text)
         return(pandoc.RawInline("tex", el.text))
     else
       return el
@@ -20,26 +19,41 @@ function Math(el)
 end
 end
 
-
 function findLatexComments(block)
     local to_return = {}
     local in_comment = false
+    local current_comment = {}
+    
+    local function flush_comment()
+        if #current_comment > 0 then
+            table.insert(to_return, pandoc.RawInline("tex", table.concat(current_comment)))
+            current_comment = {}
+        end
+    end
+    
     for _, el in ipairs(block.content) do
         if el.t == "Str" and el.text:match("^%%") then
             in_comment = true
         end
+        
         if in_comment then
             if el.text then
-                table.insert(to_return, pandoc.RawInline("tex", el.text))
+                table.insert(current_comment, el.text)
             elseif el.t == "Space" then
-                table.insert(to_return, pandoc.RawInline("tex", " "))
+                table.insert(current_comment, " ")
             elseif el.t == "SoftBreak" then
                 in_comment = false
-                table.insert(to_return, pandoc.RawInline("tex", "\n"))
+                table.insert(current_comment, "\n")
+                flush_comment()
             end
         else
+           flush_comment()
             table.insert(to_return, el)
         end
     end
+    
+    -- Flush any remaining comment at the end
+    flush_comment()
+    
     return to_return
 end
