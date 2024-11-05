f2b623c573f9fd6642d3ed692b19ac11da39d0d5
Kendra Burbank <burbank@uchicago.edu>
2024-11-05 13:20:08 -0600

    currently works, but gives a ton of sequential rawinlines

--- _extensions/examclass/latexComments.lua
+++ _extensions/examclass/latexComments.lua
@@ -22,33 +22,24 @@ end
 
 
 function findLatexComments(block)
-    local contains_comment = false
-    for i, el in ipairs(block.content) do
-        if el.t == "Str" and (el.text:match("[^\\]%%") or el.text:match("^%%")) then
-            if i == 1 or (i > 1 and block.content[i-1].t == "SoftBreak") then
-                -- Debug: Print the element type and content instead of using pandoc.write
-
-                contains_comment = true
+    local to_return = {}
+    local in_comment = false
+    for _, el in ipairs(block.content) do
+        if el.t == "Str" and el.text:match("^%%") then
+            in_comment = true
+        end
+        if in_comment then
+            if el.text then
+                table.insert(to_return, pandoc.RawInline("tex", el.text))
+            elseif el.t == "Space" then
+                table.insert(to_return, pandoc.RawInline("tex", " "))
+            elseif el.t == "SoftBreak" then
+                in_comment = false
+                table.insert(to_return, pandoc.RawInline("tex", "\n"))
             end
+        else
+            table.insert(to_return, el)
         end
     end
-    if contains_comment then
-        local result = {}
-        
-        for _, item in ipairs(block.content) do
-            if item.t == "Str" then
-                table.insert(result, pandoc.RawInline("tex", item.text))
-            elseif item.t == "Space" then
-                table.insert(result, pandoc.RawInline("tex", " "))
-            elseif item.t == "SoftBreak" then
-                table.insert(result, pandoc.RawInline("tex", "\n"))
-            else
-                table.insert(result, item)
-            end
-        end
-        table.insert(result, pandoc.SoftBreak())
-        return result
-    else
-        return block.content
-    end
+    return to_return
 end
